bd7f3b7ab35ff4a14e6c511866d38606b3918120
Santiago Dominguez <al45846@un.edu.mx>
2025-11-27 21:54:02 -0600

    cambio

---
+++ script.js
@@ -0,0 +1,8 @@
+function calcular() {
+
+    var gabinete = document.getElementById("gabinete-select").value;
+    var procesador = document.getElementById("procesador-select").value;
+    var disco = document.getElementById("disco-duro").value;
+    var ram = document.getElementById("ram-select");
+    var monitor = document.getElementById("monitor-select");
+}
